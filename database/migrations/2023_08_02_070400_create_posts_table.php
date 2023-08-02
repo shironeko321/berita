a028374c4c1d50df1c46dab6346a6e9c5cd4f944
MelayuBelitung <hzd.ah1001@gmail.com>
2023-08-02 18:44:44 +0700

    Migration

--- database/migrations/2023_08_02_070400_create_posts_table.php
+++ database/migrations/2023_08_02_070400_create_posts_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->tinyInteger('status_published');
+            $table->integer('user_id');
+            $table->integer('meta_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
--- database/migrations/2023_08_02_070821_create_post_categorys_table.php
+++ database/migrations/2023_08_02_070821_create_post_categorys_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_categorys', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid('post_id');
+            $table->integer('category_id');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_categorys');
     }
 };
