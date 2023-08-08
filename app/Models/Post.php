a90d0263c4de11a53f1580cc94b667072e3a309f
MelayuBelitung <hzd.ah1001@gmail.com>
2023-08-08 10:28:03 +0700

    update user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 
 class Post extends Model
 {
