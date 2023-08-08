873fc41a8f87f625074493c3da477f4568e6ab0e
MelayuBelitung <hzd.ah1001@gmail.com>
2023-08-08 10:24:52 +0700

    update user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status');
+            $table->integer('hak_akses');
             $table->rememberToken();
             $table->timestamps();
         });
