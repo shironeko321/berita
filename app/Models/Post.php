5f8bcf60d78571127ecc9d4fdcfc4fde9d95d43d
MelayuBelitung <hzd.ah1001@gmail.com>
2023-08-08 08:07:33 +0700

    relasi

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Tag;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 
 class Post extends Model
 {
@@ -20,4 +26,24 @@ class Post extends Model
     ];
 
     use HasFactory;
+
+    public function Tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
+
+    public function Categorys(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'post_categorys', 'post_id', 'category_id');
+    }
+
+    public function PostMeta(): HasOne
+    {
+        return $this->hasOne(PostMeta::class, 'post_meta_id', 'id');
+    }
+
+    public function User(): HasMany
+    {
+        return $this->hasMany(User::class, 'user_id', 'id');
+    }
 }
