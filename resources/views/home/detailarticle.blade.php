2b4a98e89cb72198b59241334eeeb1792172a733
bla <bla@mail.com>
2023-10-17 14:13:03 +0700

    update error

--- resources/views/home/detailarticle.blade.php
+++ resources/views/home/detailarticle.blade.php
@@ -48,7 +48,7 @@
               Tags</h5>
             <hr class="mb-4">
             @forelse ($tags as $item)
-              <a href="{{ route('category.detail', ['slug' => $item->slug]) }}" type="button"
+              <a href="{{ route('tags.detail', ['slug' => $item->slug]) }}" type="button"
                 class="inline-flex items-center justify-between px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 {{ $item->title }}
                 <span
